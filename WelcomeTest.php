5e0ff897dd5b020eb28b4692ce1f3f9605f3b292
Azman Abdullah <azmanabdullahmances@gmail.com>
2020-05-29 21:52:42 +0900

    fix 4

--- WelcomeTest.php
+++ WelcomeTest.php
@@ -1,16 +1,13 @@
 <?php
+namespace Azman;
 
 use PHPUnit\Framework\TestCase;
-
-require_once __DIR__.'/Welcome.php';
 
 class WelcomeTest extends TestCase
 {
     public function testHelloWorld()
     {
-        $wel = new Welcome();
-
-
+        $wel = new Welcome();        
         $this->assertEquals("Halo dunia", $wel->index());
     }
 }
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__.'/Welcome.php';
+require_once __DIR__.'/vendor/autoload.php';
+
+use Azman\Welcome;
 
 $welcome = new Welcome();
 
