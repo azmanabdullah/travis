5447a26618c89c22530d28e3def84e17ab5ee9af
Azman Abdullah <azmanabdullahmances@gmail.com>
2020-05-29 21:58:40 +0900

    fix 7

--- WelcomeTest.php
+++ WelcomeTest.php
@@ -9,7 +9,7 @@ class WelcomeTest extends TestCase
     public function testHelloWorld()
     {
         $wel = new Welcome();
-        
+
         $this->assertEquals("Halo dunia", $wel->index());
     }
 }
